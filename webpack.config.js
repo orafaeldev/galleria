718959eb5b632c05077f50f52e892213088d003e
Rafael Pereira <orafaeldev@gmail.com>
2026-01-04 22:05:28 -0300

    chore: update the output path by removing the galleria path

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "dist/galleria"),
+    path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
     clean: true,
     publicPath: "/galleria/",
